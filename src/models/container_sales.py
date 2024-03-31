8ffb14093c5a60a0ae2a7cfa54631051f6de466d
scmosoeu <scmosoeu@gmail.com>
2024-03-31 12:37:28 +0200

    created container sales model

--- src/models/container_sales.py
+++ src/models/container_sales.py
@@ -2,3 +2,15 @@ from pydantic import BaseModel, validator
 from typing import List
 
 from .transactions import TotalKgSold, TotalQuantitySold, TotalValueSold
+
+class ContainerSales(BaseModel):
+    commodity: str
+    total_value_sold: TotalValueSold
+    total_quantity_sold: TotalQuantitySold
+    total_kg_sold: TotalKgSold
+    quantity_available: int
+
+
+class DailyContainerSales(BaseModel):
+    information_date: str
+    daily_prices: ContainerSales
