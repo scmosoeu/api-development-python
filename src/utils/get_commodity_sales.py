1830971348c6cff3623023133c0e447d9f7d6c4a
scmosoeu <scmosoeu@gmail.com>
2024-03-31 13:54:54 +0200

    inserted index_loc

--- src/utils/get_commodity_sales.py
+++ src/utils/get_commodity_sales.py
@@ -23,9 +23,9 @@ def get_commodity_sales(commodity: str, soup: BeautifulSoup) -> CommoditySales:
 
     return CommoditySales(
         commodity=commodity.lower(),
-        total_value_sold=get_value_sold(commodity.lower(), soup),
-        total_quantity_sold=get_quantity_sold(commodity.lower(), soup),
-        total_kg_sold=get_kg_sold(commodity.lower(), soup),
+        total_value_sold=get_value_sold(commodity.lower(), 1, soup),
+        total_quantity_sold=get_quantity_sold(commodity.lower(), 2, soup),
+        total_kg_sold=get_kg_sold(commodity.lower(), 3, soup),
         quantity_available=convert_to_numeric(quantity_available, 'int')
     )
 
